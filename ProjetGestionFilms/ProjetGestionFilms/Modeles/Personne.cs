6051887080540ca48d7618b8e0fa6ab5414cb3ed
Kouame Alain N'Gouandi <kalain20@yahoo.fr>
2025-05-04 20:14:29 -0400

    Préparation de l'environnement et création de la classe Personne

--- ProjetGestionFilms/ProjetGestionFilms/Modeles/Personne.cs
+++ ProjetGestionFilms/ProjetGestionFilms/Modeles/Personne.cs
@@ -9,12 +9,12 @@ namespace ProjetGestionFilms.Modeles
     public class Personne
     {
         private int _id;
-        private string _nom;
+        private string nom;
         private string _sexe;
 
 
         public int Id { get { return _id; } set { _id = value; } }
-        public string Nom {  get { return _nom; }  set { _nom = value; } }
+        public string Nom {  get { return nom; }  set { nom = value; } }
 
         public string Sexe { get { return _sexe; } set { _sexe = value; } }
     }
